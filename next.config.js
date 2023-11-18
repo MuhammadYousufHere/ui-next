ac227824b01a234e660e9fa8878d100be0271c07
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-11-18 21:45:38 +0500

    chores: config material & optmize

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@mui/material', '@mui/icons-material'],
+  modularizeImports: {
+    '@mui/material': {
+      transform: '@mui/material/{{member}}',
+      preventFullImport: true
+    },
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+      preventFullImport: true
+    }
+  },
+  webpack: config => {
+    config.resolve.alias.canvas = false
+    return config
+  },
+  images: {
+    domains: ['localhost:3000'],
+    formats: ['image/webp', 'image/avif']
+  }
 }
 
 module.exports = nextConfig
