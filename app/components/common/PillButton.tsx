044bf00406e2345697bee1b3e5c684e24921334c
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-11-19 23:41:14 +0500

    feat: add child scale animate

--- app/components/common/PillButton.tsx
+++ app/components/common/PillButton.tsx
@@ -1,13 +1,23 @@
+'use client'
+
 import { Button, ButtonProps } from '@mui/material'
+import { motion, m, MotionProps } from 'framer-motion'
 
 interface Props extends ButtonProps {
   label: string
   isActive?: boolean
 }
+
+const childVariants = {
+  open: { opacity: 1, x: -10 },
+  close: { opacity: 0, x: 0 }
+}
+const WithMotion = motion(Button)
 export function PillButton({ label, isActive, ...rest }: Props) {
   return (
-    <Button
-      {...rest}
+    <WithMotion
+      // {...rest}
+      onClick={rest.onClick}
       variant={isActive ? 'contained' : 'outlined'}
       color='secondary'
       sx={{
@@ -19,8 +29,11 @@ export function PillButton({ label, isActive, ...rest }: Props) {
         fontSize: '0.82rem',
         textTransform: 'capitalize'
       }}
+      whileHover={{ scale: 1.05 }}
+      variants={childVariants}
+      initial='open'
     >
       {label}
-    </Button>
+    </WithMotion>
   )
 }
