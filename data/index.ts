45ee3e6ccb6ed5eb7e34313a72e6754969fba9e6
Muhammad Yousuf <khanyousufmy80@gmail.com>
2023-11-19 21:56:42 +0500

    data for dropdown

---
+++ data/index.ts
@@ -0,0 +1,92 @@
+export const contentTypes = [
+  {
+    type: 'Fun',
+    subtypes: [
+      'Board games revival',
+      'Comedy trends',
+      'Amusement parks',
+      'Escape rooms',
+      'Street performances',
+      'Online challenges',
+      'Festivals impact',
+      'Crafting movement',
+      'Social gaming',
+      'Virtual reality'
+    ]
+  },
+  {
+    type: 'Education',
+    subtypes: [
+      'Gamified learning',
+      'Education systems',
+      'Bilingual benefits',
+      'Standardized testing',
+      'Arts in schools',
+      'Tech bootcamps',
+      'Homeschooling dynamics',
+      'Mentorship importance',
+      'Educational equity',
+      'Mobile learning'
+    ]
+  },
+  {
+    type: 'Sports',
+    subtypes: [
+      'Sports medicine',
+      'Team sociology',
+      'Olympic economics',
+      'Gender parity',
+      'Fandom psychology',
+      'Extreme sports',
+      'Sports business',
+      'Youth athletics',
+      'Data analytics',
+      'Doping ethics'
+    ]
+  },
+  {
+    type: 'News',
+    subtypes: [
+      'Citizen journalism',
+      'Whistleblower influence',
+      'Fake news',
+      'Print media survival',
+      'Social media news',
+      'Cybersecurity news',
+      'Democratic media',
+      'News deserts',
+      'News economics',
+      'Broadcast evolution'
+    ]
+  },
+  {
+    type: 'Investment',
+    subtypes: [
+      'Robo-advising',
+      'Geopolitical impacts',
+      'Index funds',
+      'Real estate trends',
+      'Startup investing',
+      'Social trading',
+      'Tax strategies',
+      'Emerging markets',
+      'Financial literacy',
+      'Pension futures'
+    ]
+  },
+  {
+    type: 'Focus',
+    subtypes: [
+      'Animal oddities',
+      'Everyday science',
+      'Historical obscurities',
+      'Science myths',
+      'Human body facts',
+      'Oceanic discoveries',
+      'Space exploration',
+      'Ancient tech',
+      'World laws',
+      'Bizarre news'
+    ]
+  }
+]
